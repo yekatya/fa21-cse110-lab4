8d1ba5083482e7cde714197f19f40083a45507eb
Yekaterina Ruslanovna Kostina <47643195+yekatya@users.noreply.github.com>
2021-10-13 17:24:00 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,12 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2,
+};
 
+for (car in statistics) {
+  console.log(`${car}: ${statistics[car]}`);
+}
